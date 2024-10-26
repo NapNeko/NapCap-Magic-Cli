4d2e97bae5e5824963791e58717a12b8d62e5f07
QIAO <2550419068@qq.com>
2024-10-26 21:52:14 +0800

    ✨ feat: 初步实现下载、安装QQ

--- src/scripts/napcat-install-linux.py
+++ src/scripts/napcat-install-linux.py
@@ -8,13 +8,45 @@ import subprocess
 import sys
 import urllib.request
 from pathlib import Path
+from enum import Enum
+import dataclasses
 
 # 检查 Python 版本是否符合要求, 如果低于版本 3.8 则退出
 if sys.version_info < (3, 9):
     sys.exit("安装 NapCat 需要 Python 3.9 或更高版本")
 
 # 获取当前系统信息
-arch = platform.machine()
+arch = platform.machine().lower()
+
+
+class PackInstaller(Enum):
+    # 定义包安装器枚举
+    RPM = "rpm"
+    DPKG = "dpkg"
+
+    def __str__(self):
+        return self.value
+
+
+class PackManager(Enum):
+    # 定义管理器枚举
+    APT_GET = "apt-get"
+    YUM = "yum"
+
+    def __str__(self):
+        return self.value
+
+
+class DownloadSuffix(Enum):
+    # 定义下载链接后缀枚举
+    AMD64_DEB = "_amd64.deb"
+    AMD64_RPM = "_x86_64.rpm"
+    ARM64_DEO = "_arm64.deb"
+    ARM64_RPM = "_aarch64.rpm"
+
+    def __str__(self):
+        return self.value
+
 
 # 定义 ANSI 终端前景色的 ANSI 码
 FOREGROUND_COLORS = {
@@ -93,6 +125,61 @@ def _call_subprocess(args: list[str]) -> subprocess.CompletedProcess[str, int, b
         sys.exit(e.returncode)
 
 
+@dataclasses.dataclass
+class QQ:
+    """
+    ## QQ 相关功能
+    """
+    qq_download_url: str = None
+    package_installer: PackInstaller = None
+    package_manager: PackManager = None
+
+    def set_download_qq_url(self) -> None:
+        """
+        ## 处理 QQ 具体下载链接
+        """
+        amd64 = {PackInstaller.RPM: DownloadSuffix.AMD64_RPM, PackInstaller.DPKG: DownloadSuffix.AMD64_DEB}
+        arm64 = {PackInstaller.RPM: DownloadSuffix.ARM64_RPM, PackInstaller.DPKG: DownloadSuffix.ARM64_DEO}
+        arch_map = {"amd64": amd64, "x86_64": amd64, "arm64": arm64, "aarch64": arm64}
+
+        if arch in arch_map and self.package_installer in arch_map[arch]:
+            self.qq_download_url += arch_map[arch][self.package_installer].value
+
+    def install(self) -> None:
+        """
+        ## 下载 QQ
+        """
+        # 设置下载链接
+        self.set_download_qq_url()
+
+        # 定义下载安装参数
+        match self.package_installer:
+            # 匹配包安装器
+            case PackInstaller.RPM:
+                download_args = ['sudo', 'curl', '-L', self.qq_download_url, '-o', 'QQ.rpm']
+                install_args = ['sudo', 'yum', 'localinstall', '-y', './QQ.rpm']
+                install_file_name = 'QQ.rpm'
+
+            case PackInstaller.DPKG:
+                download_args = ['sudo', 'curl', '-L', self.qq_download_url, '-o', 'QQ.deb']
+                install_args = ['sudo', 'apt', 'install', '-f', '-y', './QQ.deb', 'libnss3', 'libgbm1', 'libasound2']
+                install_file_name = 'QQ.deb'
+
+            case _:
+                _echo(colored('red', "未知的包安装器"))
+                sys.exit(1)
+
+        # 执行下载安装任务
+        if _call_subprocess(download_args).returncode != 0:
+            _echo(colored('red', "下载 QQ 失败"))
+            sys.exit(1)
+
+        if _call_subprocess(install_args).returncode != 0:
+            _echo(colored('red', "安装 QQ 失败"))
+            sys.exit(1)
+
+
+
 class ShellInstall:
     """
     ## Shell 方式安装
@@ -102,8 +189,8 @@ class ShellInstall:
         # 定义路径
         self.base_path = Path().cwd()
 
-        # 拉版本
-        self.ger_remote_version()
+        # 拉远程版本
+        self.get_remote_version()
 
         # 清空终端, 输出 NapCat Logo
         os.system('clear')
@@ -121,9 +208,15 @@ class ShellInstall:
         """
         ## 安装 QQ
         """
-        ...
+        qq = QQ(
+            package_installer=self.package_installer,
+            package_manager=self.package_manager,
+            qq_download_url=self.qq_download_url
+        )
+        qq.install()
 
-    def ger_remote_version(self):
+
+    def get_remote_version(self) -> None:
         """
         ## 获取一些列远程版本
         """
@@ -151,9 +244,9 @@ class ShellInstall:
         ## 检测系统包管理器
         """
         if _call_subprocess(['which', 'apt-get']).returncode == 0:
-            self.package_manager = 'apt'
+            self.package_manager = PackManager.APT_GET
         elif _call_subprocess(['which', 'yum']).returncode == 0:
-            self.package_manager = 'yum'
+            self.package_manager = PackManager.YUM
         else:
             _echo(colored('red', "未检测到系统包管理器"))
             sys.exit(1)
@@ -165,9 +258,9 @@ class ShellInstall:
         ## 检测软件包安装器
         """
         if _call_subprocess(['which', 'dpkg']).returncode == 0:
-            self.package_installer = 'apt-get'
+            self.package_installer = PackInstaller.DPKG
         elif _call_subprocess(['which', 'rpm']).returncode == 0:
-            self.package_installer = 'yum'
+            self.package_installer = PackInstaller.RPM
         else:
             _echo(colored('red', "未检测到包安装器"))
             sys.exit(1)
@@ -215,6 +308,7 @@ def main() -> None:
     if args.shell:
         # 使用 shell 安装
         shell_install = ShellInstall()
+        shell_install.install_qq()
     elif args.docker:
         # 使用 Docker 安装
         _echo(colored('green', "开始使用 Docker 安装 NapCat"))
